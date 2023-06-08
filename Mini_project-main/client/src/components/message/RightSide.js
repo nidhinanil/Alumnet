5d3178c02764eeb2a92bcbe7de3191525d6ef5c8
NovaSupreme39 <nidhin.anil39@gmail.com>
2023-06-08 13:15:25 +0530

    second Commit

--- Mini_project-main/client/src/components/message/RightSide.js
+++ Mini_project-main/client/src/components/message/RightSide.js
@@ -186,11 +186,7 @@ const RightSide = () => {
                     user.length !== 0 &&
                     <UserCard user={user}>
                         <div>
-                            <i className="fas fa-phone-alt"
-                            onClick={handleAudioCall} />
-
-                            <i className="fas fa-video mx-3"
-                            onClick={handleVideoCall} />
+                           
 
                             <i className="fas fa-trash text-danger"
                             onClick={handleDeleteConversation} />
